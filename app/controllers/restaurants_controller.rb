3ed0787a085af08d85970a82d0fefb3876ae7ba6
Samuel Coizet <messa57fr@gmail.com>
2022-07-01 12:14:43 +0200

    new restaurant ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,20 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+    @categories = Category.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_restaurant
@@ -15,7 +29,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :description)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :description, :category_id)
   end
 
 end
